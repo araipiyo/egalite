cfd7f60834ef4ecc46058c17f593aea8daf6ac1c
Shunichi Arai <arai@mogura.co.jp>
2009-06-01 05:49:50 +0000

    * now works with Sequel 3.0.0

--- egalite/sequel_helper.rb
+++ egalite/sequel_helper.rb
@@ -3,12 +3,12 @@ class Sequel::Model
   def update_with(hash, *selection)
     data = {}
     selection.flatten.each { |k| data[k] = hash[k] || hash[k.to_sym] }
-    update_with_params(data)
+    update(data)
   end
   def update_without(hash, *selection)
     selection.flatten.each { |k| hash.delete(k) if hash[k] }
     selection.flatten.each { |k| hash.delete(k.to_sym) if hash[k.to_sym] }
-    update_with_params(hash)
+    update(hash)
   end
   def to_hash
     hash = {}
