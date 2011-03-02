c1409ce773bda70e7bd32eef8ab83c0489a17b27
Shunichi Arai <arai@mogura.co.jp>
2011-03-03 04:13:18 +0900

    sendmail.rb is now 99% ready.

--- sendmail.rb
+++ sendmail.rb
@@ -17,23 +17,25 @@ require 'time'
 #   :subject => '',
 # }
 
-class Sendmail
+module Sendmail
+  class QualifiedMailbox < String
+  end
  class <<self
   def folding(h, s) # folding white space. see RFC5322, section 2.3.3 and 3.2.2.
     len = 78 - h.size - ": ".size
     len2nd = 78 - " ".size
     lines = []
     line = ""
-    s.split(/\s+/).each { |c| # each word (according to gmail's behavior)
+    s.strip.split(/\s+/).each { |c| # each word (according to gmail's behavior)
       if (line+c).size > len
         len = len2nd
-        lines << line.sub(/\s+\Z/,'')
+        lines << line.strip
         line = c + " "
       else
         line << c + " "
       end
     }
-    lines << line.sub(/\s+\Z/,'') if line.size > 0
+    lines << line.strip if line.size > 0
     lines.join("\n ")
   end
   def multibyte_folding(h, s, encoding = 'UTF-8') # RFC2047
@@ -42,7 +44,7 @@ class Sendmail
     len2nd = 76 - bracketsize
     lines = []
     line = ""
-    s = s.gsub(/\s+/, ' ')
+    s = s.gsub(/\s+/, ' ').strip
     s.split(//).each { |c| # each character (including multi-byte ones)
       teststr = line+c
       teststr = NKF.nkf('-Wj',teststr) if encoding =~ /iso-2022-jp/i
@@ -67,15 +69,18 @@ class Sendmail
   def quote_string(s)
     '"' + vchar(wsp(s)).gsub(/\\/,"\\\\\\").gsub(/\"/,'\\\"') + '"'
   end
+  def multibyte?(s)
+    s.each_byte.any? { |c| c > 0x7f }
+  end
   def encode_phrase(header, s)
-    if s.each_byte.any? { |c| c > 0x7f }
+    if multibyte?(s)
       multibyte_folding(header, s)
     else
       folding(header, quote_string(s))
     end
   end
   def encode_unstructured(header, s)
-    if s.each_byte.any? { |c| c > 0x7f }
+    if multibyte?(s)
       multibyte_folding(header, s)
     else
       folding(header, vchar(wsp(s)))
@@ -105,11 +110,11 @@ class Sendmail
       [local, domain]
     end
   end
-  def address(addrspec, name = nil, header='Reply-to')
-    # no support for group mail, mailbox-list and address-list.
+  def address(addrspec, name = nil, header='Reply-to') 
+    # mailbox in RFC5322 section 3.4. not 'address' as in RFC.
     raise 'invalid mail address.' unless parse_addrspec(addrspec)
     if name and name.size > 0
-      "#{encode_phrase(header, name)}\n <#{addrspec}>" # folded style
+      QualifiedMailbox.new(folding(header, "#{encode_phrase(header, name)} <#{addrspec}>"))
     else
       addrspec
     end
@@ -123,25 +128,84 @@ class Sendmail
       send(subject,text,to,from)
     }
   end
-  def send(subject,text,to,from,from_name,to_name,envelope_from)
-    text = [text].pack('m')
-    subject = NKF.nkf("-WwMm0", subject)
-    from_name = NKF.nkf("-WwMm0", from_name)
-    to_name = NKF.nkf("-WwMm0", to_name)
-    to = [to].flatten
-    headers = "MIME-Version: 1.0\n"
-    headers << "Date: #{Time.now.rfc822}\n"
-    headers << "Subject: #{subject}\n"
-    headers << "Content-Type: text/plain; charset=UTF-8\n"
-    headers << "Content-Transfer-Encoding: base64\n"
-    headers << "From: #{from_name} <#{from}>\n"
-    headers << "To: #{to_name} <#{to[0]}>\n"
-    headers << "\n"
-    text = "#{headers}#{text}"
-    Net::SMTP.start('localhost') { |smtp|
-      from = envelope_from if envelope_from
-      smtp.send_message(text,from, to)
+  def mailboxlist(value, header = 'Reply-to')
+    case value
+      when QualifiedMailbox: value
+      when String: parse_addrspec(value) ? value : nil
+      when Hash: address(value[:address],value[:name],header)
+      when Array
+        folding(header, value.map { |v|
+          v.is_a?(Array) ? address(v[0],v[1]) : mailboxlist(v,header)
+        }.join(', '))
+      else
+        nil
+    end
+  end
+  def message(body, params)
+    headers = {}
+    
+    raise "From must be exist." unless params[:from]
+    raise "The number of sender must be zero or one." if params[:sender].is_a?(Array) and params[:sender].size > 1
+    raise "When the number of 'from' is more than one, sender must be exist" if params[:from].is_a?(Array) and params[:from].size > 1 and not params[:sender]
+    
+    %w[From To Sender Reply-To Cc].each { |s|
+      v = params[s.gsub(/-/,'_').downcase.to_sym]
+      headers[s] = mailboxlist(v,s) if v and v.size >= 1
     }
+    
+    headers["Subject"] = encode_unstructured("Subject",params[:subject].to_s) if params[:subject]
+    headers["MIME-Version"] = "1.0"
+    date = params[:date] || Time.now
+    headers["Date"] = date.is_a?(Time) ? date.rfc822 : date
+    headers["Content-Type"] = "text/plain; charset=UTF-8"
+    
+    if multibyte?(body)
+      headers["Content-Transfer-Encoding"] = "base64"
+      body = [body].pack('m')
+    else
+      headers["Content-Transfer-Encoding"] = "7bit"
+    end
+    
+    text = [headers.map{|k,v| "#{k}: #{v}"}.join("\n"),body].join("\n\n")
+  end
+  private
+  def _ta(value)
+    case value
+      when QualifiedMailbox
+        value =~ /<(#{atext_loose}+?@#{atext_loose}+?)>\Z/
+        $1
+      when String: parse_addrspec(value) ? value : nil
+      when Hash: parse_addrspec(value[:address]) ? value[:address] : nil
+      when Array
+        value.map { |v|
+          if v.is_a?(Array)
+            parse_addrspec(v[0]) ? v[0] : nil
+          else
+            _ta(v)
+          end
+        }
+      else nil
+    end
+  end
+  public
+  def to_addresses(params)
+    addresses = [:to, :cc, :bcc].map { |s|
+      _ta(params[s])
+    }
+    addresses.flatten.compact.uniq
+  end
+  def _send(text, envelope_from, to, host = 'localhost')
+    Net::SMTP.start(host) { |smtp|
+      smtp.send_message(text, envelope_from, to)
+    }
+  end
+  def send(body, params, envelope_from = nil, host = 'localhost')
+    _send(
+      message(body, params),
+      envelope_from || params[:sender] || params[:from],
+      to_addresses(params),
+      host
+    )
   end
  end
 end
--- test/test_sendmail.rb
+++ test/test_sendmail.rb
@@ -13,6 +13,8 @@ class T_Sendmail < Test::Unit::TestCase
     assert_equal 70, a[1].size
     assert_match /\A\s[0-9\s]{69}\Z/, a[1]
     assert_equal 200-70-70, a[2].size
+    s2 = Sendmail.folding('To', s)
+    assert_equal s, s2
   end
   def test_encode_phrase
     s = Sendmail.encode_phrase('To', "0123\"6\\9\x00"*20)
@@ -41,9 +43,9 @@ class T_Sendmail < Test::Unit::TestCase
   end
   def test_address
     s = Sendmail.address('foo@example.com', '新井 俊一', 'To')
-    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\=\n <foo@example.com>\Z/, s
+    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\= <foo@example.com>\Z/, s
     s = Sendmail.address('foo@example.com', "\x00ARAI \n Shunichi", 'To')
-    assert_match /"ARAI Shunichi"\n <foo@example.com>\Z/, s
+    assert_match /"ARAI Shunichi" <foo@example.com>\Z/, s
   end
   def test_multibyte_folding
     s = Sendmail.multibyte_folding('Subject', 'あいうえお'*20)
@@ -56,6 +58,89 @@ class T_Sendmail < Test::Unit::TestCase
       $1.unpack('m')[0]
     }.join
     assert_equal 'あいうえお'*20, s2
+  end
+  def test_mailboxlist
+    s = Sendmail.mailboxlist('foo@example.com')
+    assert_equal 'foo@example.com',s
+  end
+  def parse_message(s)
+    (header, body) = s.split(/\n\n/,2)
+    headers = {}
+    header.gsub(/\n[\s\t]+/, ' ').split(/\n/).each { |line|
+      (k,v) = line.split(/: +/,2)
+      headers[k] = v
+    }
+    [headers, body]
+  end
+  def test_message_7bit
+    s = Sendmail.message('hoge',:from=>'hoge@example.com',:subject=>'test')
+    (h,b) = parse_message(s)
+    assert_equal 'hoge', b
+    assert_equal '1.0', h['MIME-Version']
+    assert Time.rfc822(h['Date'])
+    assert_equal '7bit', h['Content-Transfer-Encoding']
+    assert_equal 'text/plain; charset=UTF-8', h['Content-Type']
+    assert_equal 'test', h['Subject']
+  end
+  def test_message_multibyte
+    s = Sendmail.message('あいうえお',:from=>'hoge@example.com')
+    (h,b) = parse_message(s)
+    assert_equal 'あいうえお', b.unpack('m')[0]
+    assert_equal '1.0', h['MIME-Version']
+    assert Time.rfc822(h['Date'])
+    assert_equal 'base64', h['Content-Transfer-Encoding']
+    assert_equal 'text/plain; charset=UTF-8', h['Content-Type']
+  end
+  def params
+    {
+      :date => Time.local(0),
+      :from => 'hoge@example.com',
+      :to   => [Sendmail.address('arai@example.com','新井俊一'),
+                ['tanaka@example.com','田中太郎'],
+                {:address => 'takeda@example.com', :name => '武田一郎'},
+                'ueno@example.com'
+               ],
+      :cc   => Sendmail.address('foo@example.com', 'Foo Bar'),
+      :bcc  => Sendmail.address('zzz@example.com', 'zzz'),
+      :reply_to=> Sendmail.address('baz@example.com', 'Baz Bzz'),
+      :subject => 'こんにちは',
+    }
+  end
+  def test_message_headers
+    assert_raise(RuntimeError) { Sendmail.message('',{}) }
+    assert_raise(RuntimeError) { Sendmail.message('',{:sender => ['1','2']}) }
+    assert_raise(RuntimeError) { Sendmail.message('',{:from => [1,2,3]}) }
+    s = Sendmail.message('あいうえお',params)
+    (h,b) = parse_message(s)
+    assert_equal 'あいうえお', b.unpack('m')[0]
+    assert_equal '1.0', h['MIME-Version']
+    assert_equal Time.local(0), Time.rfc822(h['Date'])
+    assert_equal 'base64', h['Content-Transfer-Encoding']
+    assert_equal 'text/plain; charset=UTF-8', h['Content-Type']
+    assert_equal 'hoge@example.com', h['From']
+    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\=\n?\Z/, h['Subject']
+    h['Subject'] =~ /\A\=\?UTF-8\?B\?(.+?)\?\=/
+    assert_equal 'こんにちは', $1.unpack('m')[0]
+    to = h['To']
+    tos = to.split(/\s*,\s+/)
+    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\= <arai@example.com>\Z/, tos[0]
+    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\= <tanaka@example.com>\Z/, tos[1]
+    assert_match /\A\=\?UTF-8\?B\?(.+?)\?\= <takeda@example.com>\Z/, tos[2]
+    assert_match /\Aueno@example.com\Z/, tos[3]
+    to =~ /\A\=\?UTF-8\?B\?(.+?)\?\=.+?\=\?UTF-8\?B\?(.+?)\?\=.+?\=\?UTF-8\?B\?(.+?)\?\=/
+    (a,b,c) = [$1,$2,$3].map { |s| s.unpack('m')[0] }
+    assert_match '新井俊一', a
+    assert_match '田中太郎', b
+    assert_match '武田一郎', c
+    assert_match '"Foo Bar" <foo@example.com>', h['Cc']
+    assert_match '"Baz Bzz" <baz@example.com>', h['Reply-To']
+    assert_nil h['Bcc']
+  end
+  def test_to_addresses
+    a = Sendmail.to_addresses(params)
+    %w[foo@example.com arai@example.com tanaka@example.com takeda@example.com ueno@example.com zzz@example.com].each { |s|
+      assert a.include?(s)
+    }
   end
 end
 
