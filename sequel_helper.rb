ff09b53c24760721d2c367c23802625998bf9e2d
Shunichi Arai <arai@mogura.co.jp>
2011-04-26 10:03:58 +0900

    Fixed that the hash parameter was broken by update_without.

--- sequel_helper.rb
+++ sequel_helper.rb
@@ -6,6 +6,7 @@ class Sequel::Model
     update(data)
   end
   def update_without(hash, *selection)
+    hash = hash.clone
     selection.flatten.each { |k| hash.delete(k.to_s) if hash[k.to_s] }
     selection.flatten.each { |k| hash.delete(k.to_sym) if hash[k.to_sym] }
     update(hash)
