d40a52a4fecfb7a952e51f1eb1835b8731d60604
Shunichi Arai <arai@mellowtone.co.jp>
2016-04-14 15:39:20 +0900

    added http.rb

---
+++ lib/egalite/http.rb
@@ -0,0 +1,71 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+
+module Egalite
+  module HTTP
+    def self.parse_url(url, options)
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == "https"
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+      [http, uri]
+    end
+    def self.parse_options(options)
+      if options[:basic_auth]
+        u = options[:basic_auth][0]
+        pw = options[:basic_auth][1]
+        b = ["#{u}:#{pw}"].pack("m")
+        options[:header] ||= {}
+        options[:header]["Authorization"] = "Basic #{b}".chop
+      end
+    end
+    def self.parse_response(response)
+      ret = {}
+      ret[:body] = response.body
+      ret[:headers] = response.each {}
+      ret[:headers] = Hash[ret[:headers].map { |k,v|
+        [k.tr("-","_").downcase.to_sym,v[0]]
+      }]
+      ret[:headers][:content_length] = ret[:headers][:content_length].to_i
+      ret[:headers][:date] = Time.parse(ret[:headers][:date]) rescue ret[:headers][:date]
+      ret[:code] = response.code.to_i
+      ret
+    end
+    def self.get(url, options = {})
+      params = options[:params]
+      if params.is_a?(Hash)
+        params = params.map { |k,v|
+          "#{k}=#{v}"
+        }.join("&")
+      end
+      if params.is_a?(String)
+        if url =~ /\?/
+          url << "&"
+        else
+          url << "?"
+        end
+        url << params
+      end
+      parse_options(options)
+      (http, uri) = parse_url(url, options)
+      resp = http.get(uri.request_uri, options[:header])
+      parse_response(resp)
+    end
+    def self.post(url, body = nil, options = {})
+      uri = parse_url(url,options)
+      if body.is_a?(Hash)
+        body = body.map { |k,v|
+          "#{k}=#{v}"
+        }.join("&")
+      end
+      parse_options(options)
+      (http, uri) = parse_url(url, options)
+      resp = http.post(uri.request_uri, body, options[:header])
+      parse_response(resp)
+    end
+  end
+end
+
